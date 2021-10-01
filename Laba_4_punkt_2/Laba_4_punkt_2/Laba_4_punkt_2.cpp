7dcab7fd660ccd1ad15de6f69932072ed7e80eb6
Valeriana11 <90962263+Valeriana11@users.noreply.github.com>
2021-10-02 02:10:11 +0300

    Added punkt 1 and 2

---
+++ Laba_4_punkt_2/Laba_4_punkt_2/Laba_4_punkt_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+//Пункт 2
+double D;
+double Pi = 3.14;
+
+cout << "Wwedite diametr okruzhnosti" << endl;
+
+cin >> D;
+
+cout << "Dlina okruzhnosti = " << Pi * D << endl;
+
+return 0;
+}
