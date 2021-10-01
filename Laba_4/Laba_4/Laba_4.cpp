b565b8e1907e4cae041ea2a450256ee72b3f7917
Valeriana11 <90962263+Valeriana11@users.noreply.github.com>
2021-10-02 00:59:53 +0300

    Added 1 to lab 4

--- Laba_4/Laba_4/Laba_4.cpp
+++ Laba_4/Laba_4/Laba_4.cpp
@@ -1,1 +1,18 @@
-﻿
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+	double a, b;
+
+	cout << "Wwedite dliny storon pryamoygolnika a i b" << endl;
+	
+	cin >> a;
+	cin >> b;
+
+	cout << "Ploshad = " << a * b << endl;
+
+	cout << "Pirimetr = " << 2 * (a + b) << endl;
+
+	return 0;
+}
