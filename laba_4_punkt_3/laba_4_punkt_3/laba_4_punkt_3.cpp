ede3496ff2fceb30901a6ceeb8b01206a0acdfba
Valeriana11 <90962263+Valeriana11@users.noreply.github.com>
2021-10-02 02:47:45 +0300

    laba 4 punkt 3

---
+++ laba_4_punkt_3/laba_4_punkt_3/laba_4_punkt_3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+  double a,b;
+  cout << " Nayti srednee arifmeticheskoe" << "vvedite a i b" << endl;
+  cin >> a >> b;
+  cout << "Srednee arefmeticheskoe a i b = " << (a + b) / 2 << endl;
+  return 0;
+
+}
+
