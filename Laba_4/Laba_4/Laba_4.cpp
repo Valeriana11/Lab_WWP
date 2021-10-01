3e9f460ed45a303af42e25008023d73bb4528880
Valeriana11 <90962263+Valeriana11@users.noreply.github.com>
2021-10-02 01:49:44 +0300

    added 2 to lab 4

--- Laba_4/Laba_4/Laba_4.cpp
+++ Laba_4/Laba_4/Laba_4.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+	//Пункт 1
 	double a, b;
 
 	cout << "Wwedite dliny storon pryamoygolnika a i b" << endl;
@@ -12,7 +13,17 @@ int main()
 
 	cout << "Ploshad = " << a * b << endl;
 
-	cout << "Pirimetr = " << 2 * (a + b) << endl;
+	cout << "Pirimetr = " << 2 * (a + b) << endl << endl;
+
+	//Пункт 2
+	double D;
+	double Pi = 3.14;
+
+	cout << "Wwedite diametr okruzhnosti" << endl;
+
+	cin >> D;
+
+	cout << "Dlina okruzhnosti = " << Pi * D << endl;
 
 	return 0;
 }
