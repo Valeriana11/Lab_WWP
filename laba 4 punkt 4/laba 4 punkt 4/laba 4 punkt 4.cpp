b69ffec50eb8c40f0da0548e77d188bd288c71ba
Valeriana11 <90962263+Valeriana11@users.noreply.github.com>
2021-10-02 03:15:59 +0300

    laba 4 punkt 4

---
+++ laba 4 punkt 4/laba 4 punkt 4/laba 4 punkt 4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, b;
+
+	cout << " Naiti summy, raznost, proizwedenie i chasnoe a i b" << endl << " Vvedite a i b" << endl;
+	cin >> a >> b;
+
+	a = a * a;
+	b = b * b;
+
+	cout << "Summa kvadratovo = " << a + b << endl << "Raznost kvadratov = " << a - b << endl << "Proizvedenie kvadratov = " << a * b << endl << "Chasnoe kvadratov = " << a / b << endl;
+
+	return 0;
+}
