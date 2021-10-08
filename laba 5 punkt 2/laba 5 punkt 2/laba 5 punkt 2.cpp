f50196f5a85178fa164b0812fcd178ff5594fac8
Valeriana11 <90962263+Valeriana11@users.noreply.github.com>
2021-10-08 21:47:01 +0300

    laba 5 punkt 2

---
+++ laba 5 punkt 2/laba 5 punkt 2/laba 5 punkt 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{ 
+    double A, B, C;
+    cout << "Dany tri tochki A, B, C na chislovoj osi. Najti dliny otrezkov AC i BC i ih summu" << endl;
+    cout << "Vvedite 3 koordinaty tochek na chislovoj osi cherez zapyatuyu v vide: A, B, C "<< endl;
+    cin >> A >> B >> C;
+    double AC = abs(A - C);
+    double BC = abs(B - C);
+    double ACBC = AC + BC;
+
+    cout <<"dlina otrezka AC = " << AC << " dlina otrezka BC = " << BC << " summa AC i BC = " << ACBC << endl;
+
+    return 0;
+}
