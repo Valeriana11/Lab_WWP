05eef59ae2e19ff71f7c0ac4a3d367d3990ecffc
Valeriana11 <90962263+Valeriana11@users.noreply.github.com>
2021-11-17 01:40:25 +0300

    laba 5 , 6

--- laba 5/laba 5/laba 5.cpp
+++ laba 5/laba 5/laba 5.cpp
@@ -1,18 +1,81 @@
 ﻿#include <iostream>
-using namespace std;
+#include <Windows.h>
+#include <cmath>
+
 int main()
 {
-    double x1, x2, y1, y2, distance;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    cout << "vvedite koordinaty x1:" << endl;
-    cin >> x1;
-    cout << "vvedite koordinaty y1:" << endl;
-    cin >> y1;
-    cout << "vvedite koordinaty x2:" << endl;
-    cin >> x2;
-    cout << "vvedite koordinaty y2:" << endl;
-    cin >> y2;                              distance = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));      // формула для вычисления расстояния 
-    cout << "rasstoyanie meghdy dvymya toshami:" << distance << endl;
+    double x1, y1;
+    double  x2, y2;
+    double x3, y3;
 
-    return 0;
+    double A, C, B;
+
+    //1
+    std::cout << "Найти расстояние между двумя точками с заданными координатами. \n";
+    std::cout << "Введите 4 координаты через пробел в виде: x1, y1, x2, y2 \n";
+
+    std::cin >> x1 >> y1 >> x2 >> y2;
+
+    double dist = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+    std::cout << "Расстояние между двумя точками с заданными координатами = " << dist << "\n\n";
+
+    //2
+    std::cout << "Даны три точки A, B, C на числовой оси. Найти длины отрезков AC и BC и их сумму.\n";
+    std::cout << "Введите 3 координаты точек на числовой оси через запятую в виде: A, B, C.\n";
+
+    std::cin >> A >> B >> C;
+
+    double AC = abs(A - C);
+    double BC = abs(B - C);
+    double ACBC = AC + BC;
+
+
+    std::cout << "Длина отрезка АС = " << AC << " Длина отрезка ВС = " << BC << " Сумма АС и ВС = " << ACBC << "\n\n";
+
+    //3
+    std::cout << "Даны три точки A, B, C на числовой оси. Точка C расположена между точками A и B.\nНайти произведение длин отрезков AC и BC.\n";
+    std::cout << "Введите 3 координаты точек на числовой оси через запятую в виде: A, B, C.\n";
+
+    std::cin >> A >> B >> C;
+
+    AC = abs(A - C);
+    BC = abs(B - C);
+
+
+    std::cout << "Произведение длин отрезков AC и BC = " << AC * BC << "\n\n";
+
+    //4
+    std::cout << "Даны координаты двух противоположных вершин прямоугольника: (x1, y1), (x2, y2).\nСтороны прямоугольника параллельны осям координат.\n";
+    std::cout << "Введите 4 координаты через пробел в виде: x1, y1, x2, y2 \n";
+
+    std::cin >> x1 >> y1 >> x2 >> y2;
+
+    double edge_x = abs(x2 - x1);
+    double edge_y = abs(y2 - y1);
+
+    double S = edge_x * edge_y;
+    double P = (edge_x + edge_y) * 2;
+
+    std::cout << "Периметр прямоугольника = " << P << " Площадь равна = " << S << "\n\n";
+
+    //5
+    std::cout << "Даны координаты трех вершин треугольника: (x1, y1), (x2, y2), (x3, y3).\n";
+    std::cout << "Введите 6 координат через пробел в виде: x1, y1, x2, y2, x3, y3\n";
+
+    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    double side_1 = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    double side_2 = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+    double side_3 = sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+
+    P = side_1 + side_2 + side_3;
+
+    double p = P / 2;
+    S = sqrt(p * (p - side_1) * (p - side_2) * (p - side_3));
+
+    std::cout << "Площадь треугольника = " << S << " Периметр треугольника = " << P << "\n\n";
 }
---
+++ laba 6/laba 6/laba 6.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+int main()
+{
+
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double A, B, C;
+    double temp1, temp2;
+
+    //1
+    std::cout << "Поменяeм местами содержимое переменных A и B\n";
+    std::cout << "Введите значение переменной А\n";
+    std::cin >> A; //1
+    std::cout << "Введите значение переменной B\n";
+    std::cin >> B; //0
+
+    temp1 = A;
+    A = B;
+    B = temp1;
+
+    std::cout << "Значение переменной А - " << A << " Значение переменной В - " << B << "\n\n";
+
+    //2
+    std::cout << "Поменяeм местами содержимое переменных A, B и С. A в B, B — в C, C — в A\n";
+    std::cout << "Введите значение переменной А\n";
+    std::cin >> A;
+    std::cout << "Введите значение переменной B\n";
+    std::cin >> B;
+    std::cout << "Введите значение переменной C\n";
+    std::cin >> C;
+
+    temp1 = B;
+    B = A;
+    temp2 = C;
+    C = temp1;
+    A = temp2;
+
+    std::cout << "Значение переменной А - " << A << " Значение переменной В - " << B << " Значение переменной C - " << C << "\n\n";
+
+    //3
+    std::cout << "Поменяeм местами содержимое переменных A, B и С. A в C, C — в B, B — в A\n";
+    std::cout << "Введите значение переменной А\n";
+    std::cin >> A;
+    std::cout << "Введите значение переменной B\n";
+    std::cin >> B;
+    std::cout << "Введите значение переменной C\n";
+    std::cin >> C;
+
+    temp1 = C;
+    C = A;
+    temp2 = B;
+    B = temp1;
+    A = temp2;
+
+    std::cout << "Значение переменной А - " << A << " Значение переменной В - " << B << " Значение переменной C - " << C << "\n\n";
+
+    //4
+    double y, x = 0;
+
+    std::cout << "Найдем значение выражения y = 3x^6 - 6x^2 - 7\n";
+    std::cout << "Введите значение x\n";
+    std::cin >> x;
+
+    y = 3.0 * pow(x, 6) - 6.0 * pow(x, 2) - 7;
+
+    std::cout << "Ответ: " << y << "\n\n";
+
+    //5
+    std::cout << "Найдем значение выражения y = 4(x-3)^6 - 7(x-3)^3 + 2 \n";
+    std::cout << "Введите значение x\n";
+    std::cin >> x;
+
+    y = 4 * pow(x - 3, 6) - 7 * pow(x - 3, 3) + 2;
+
+    std::cout << "Ответ: " << y << "\n\n";
+
+    //6
+    double a;
+    double powa;
+
+    std::cout << "Введите значение А\n";
+    std::cin >> a;
+
+    powa = a * a;
+
+    std::cout << "Значение А в 8 степени = " << powa * powa * powa * powa << "\n\n";
+
+    //7
+    std::cout << "Введите значение А\n";
+    std::cin >> a;
+
+    powa = a * a;
+    int powpowa = powa * powa;
+
+    std::cout << "Значение А в 15 степени = " << powpowa * powpowa * powpowa * a * a * a << "\n\n";
+}
